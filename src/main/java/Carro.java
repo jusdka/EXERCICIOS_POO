45937f79f1178ad9c0f676ef21b7b4cb2cbcf07a
Kamila Pereira Da Silva Pedroso <kamilabts503@gmail.com>
2026-03-24 22:51:15 -0400

    Exercicio 4

--- src/main/java/Carro.java
+++ src/main/java/Carro.java
@@ -1,18 +1,38 @@
 public class Carro {
-    String marca;
-    String modelo;
-    int ano;
+    private String marca;
+    private String modelo;
+    private int ano;
 
-    public int idadeDoCarro(){
-        return 2026 - ano;
+    public String getMarca(){
+        return this.marca;
+    }
+    public void setMarca(String m){ //void pq ele não vai retornar nada
+        this.marca = m;
+    }
+
+    public String getModelo(){
+        return this.modelo;
+    }
+    public void setModelo(String md){
+        this.modelo = md;
+    }
+
+    public int getAno(){
+        return this.ano;
+    }
+    public void setAno(int a){
+        // validação
+        if (a < 1886 || a > 2026){
+            System.out.println("404 ! Ano inválido");
+        } else{
+            this.ano = a;
+        }
     }
 
     public void exibir(){
-        System.out.println("Marca: " + this.marca);
-        System.out.println("Modelo: " + this.modelo);
-        System.out.println("Ano: " + this.ano);
-        System.out.println("A idade do carro é: " + idadeDoCarro());
-
+        System.out.println("Marca: " + this.getMarca());
+        System.out.println("Modelo: " + this.getModelo());
+        System.out.println("Ano: " + (this.getAno() == 0 ? "Não informado" : this.getAno()));
     }
 
 }
--- src/main/java/Garagem.java
+++ src/main/java/Garagem.java
@@ -1,36 +1,17 @@
-import java.util.ArrayList;
+
 public class Garagem {
     public static void main(String[] args){
-
-        ArrayList<Carro> listadecarros = new ArrayList<>();
-        //arraylist - array dinâmico; <Carro> - apenas objs da classe carro; listadecarros - apenas um nome para identificar
-        //a coleção; new Arraylist<>() - criando a lista na memória do pc (new - reserva espaço; <> - avisa ao Java para inferir que o tipo é Carro
-
         Carro carro1 = new Carro();
-        carro1.marca = "BYD";
-        carro1.modelo = "Dolphin";
-        carro1.ano = 2024;
-        listadecarros.add(carro1); // adicionou o carro na lista
+        carro1.setMarca("BYD");
+        carro1.setModelo("Dolphin");
+        carro1.setAno(2025);
+        carro1.exibir();
 
         Carro carro2 = new Carro();
-        carro2.marca = "VolksWagen";
-        carro2.modelo = "Voyage";
-        carro2.ano = 2011;
-        listadecarros.add(carro2);
-
-        Carro carro3 = new Carro();
-        carro3.marca = "Toyota";
-        carro3.modelo = "Corolla";
-        carro3.ano = 2016;
-        listadecarros.add(carro3);
-
-        // percorrendo a lista usando for-each
-        System.out.println("*** LISTA DE CARROS ***");
-        //percorrer coleções em java (percorrer a lista)
-        for (Carro c : listadecarros){ // para cada carro dentro da 'listadecarros' exiba...
-            c.exibir();// o carros vão ser mandandos para o c para serem exibidos (1 volta - carro1 e assim por diante)
-            System.out.println("***********************");
-        }
+        carro2.setMarca("Toyota");
+        carro2.setModelo("Corolla");
+        carro2.setAno(1700);
+        carro2.exibir();
 
     }
 
