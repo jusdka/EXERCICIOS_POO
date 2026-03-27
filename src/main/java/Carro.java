37aa0eed4995034d9ec7f642af2c8f33979605db
Kamila Pereira Da Silva Pedroso <kamilabts503@gmail.com>
2026-03-26 21:49:15 -0400

    Exercicio 5

--- src/main/java/Carro.java
+++ src/main/java/Carro.java
@@ -3,6 +3,11 @@ public class Carro {
     private String modelo;
     private int ano;
 
+    public Carro(String marca, String modelo, int ano){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+    }
     public String getMarca(){
         return this.marca;
     }
--- src/main/java/Garagem.java
+++ src/main/java/Garagem.java
@@ -1,18 +1,17 @@
 
 public class Garagem {
     public static void main(String[] args){
-        Carro carro1 = new Carro();
-        carro1.setMarca("BYD");
-        carro1.setModelo("Dolphin");
-        carro1.setAno(2025);
+        Carro carro1 = new Carro("VolksWagen", "Nivus", 2023);
         carro1.exibir();
 
-        Carro carro2 = new Carro();
-        carro2.setMarca("Toyota");
-        carro2.setModelo("Corolla");
-        carro2.setAno(1700);
+        Carro carro2 = new Carro("BYD", "Mini Dolphin", 2025);
         carro2.exibir();
 
     }
 
 }
+
+//qual a vantagem de usar construtor em vez de chamar setters um
+//por um?
+
+//A vantagem é que o construtor ja define o estado inicial e é executado automaticamente assim que o objeto é criado.
