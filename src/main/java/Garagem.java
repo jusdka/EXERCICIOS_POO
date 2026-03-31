2859f6bd4d4e6aa0e1c2bddfb85259d5f8e293f1
Kamila Pereira Da Silva Pedroso <kamilabts503@gmail.com>
2026-03-30 22:58:44 -0400

    Exercicio 10

--- src/main/java/Garagem.java
+++ src/main/java/Garagem.java
@@ -1,23 +1,23 @@
 
 public class Garagem {
-    public static void main(String[] args){
-        Carro carro1 = new Carro("VolksWagen", "Nivus", 2023);
-        Carro carro2 = new Carro();
-        Carro carro3 = new Carro("Chevrolet", "Onix", 2020);
-        Carro carro4 = new Carro();
-        Carro carro5 = new Carro("Fiat", "Mobi", 2021);
-        System.out.println("--------------------------------------------");
-        Carro.imprimirTotal();
+   public static void main(String[] args){
+       //public static void main(String[] args) {
+           //if (true) {
+               //int x = 10;
+           //}
+           //System.out.println(x);
+       //}
+       //o código não é compilado, pois o compilador diz que não há um simbolo encontrado.
 
-        //Carro.ANO_PRIMEIRO_CARRO = 1900;
-        System.out.println("Ano primeiro carro: " + Carro.ANO_PRIMEIRO_CARRO);
-
-
-    }
-
+       int x = 0;
+       if (true){
+           x = 10;
+       }
+       System.out.println(x);
+   }
 }
+// O escopo é a região do código onde uma variável é "visível" e pode ser acessada.
+// No caso do Java, o escopo é delimitado por chaves {}.
 
-// se tentar alterar o valor para 1900 ele não vai deixar pois é uma variável final.
+//A variável x não existia fora do if, pois como ela está dentro das chaves, ela vira uma variável local daquele bloco.
 
-//FINAL: adicionar final em uma variável faz ela virar uma constante de leitura única, ou seja, seu valor não pode ser
-// alterado.
