96c3b00ea09c7e6a8c4d44fd179b219478aacb5c
Kamila Pereira Da Silva Pedroso <kamilabts503@gmail.com>
2026-04-03 17:32:19 -0400

    Exercicio12

--- src/main/java/Garagem.java
+++ src/main/java/Garagem.java
@@ -1,27 +1,20 @@
 
 public class Garagem {
    public static void main(String[] args){
-       //public static void main(String[] args) {
-           //if (true) {
-               //int x = 10;
-           //}
-           //System.out.println(x);
-       //}
-       //o código não é compilado, pois o compilador diz que não há um simbolo encontrado.
-
-       int x = 0;
-       if (true){
-           x = 10;
-       }
-       System.out.println(x);
-
-       int a = 10;
-       int b = a;
-       b = 99;
-       System.out.println("a = " + a);
-       System.out.println("b = " + b);
+       Carro c1 = new Carro("Fiat", "Uno", 2020);
+       Carro c2 = c1;
+       c2.setModelo("Palio");
+       System.out.println(c1.getModelo());
+       System.out.println(c2.getModelo());
    }
 }
+// o modelo de c1 e c2 é Palio.
 
-//a ficou valendo 10 e b ficou valendo 99.
-//alterar o B não mudou o A pois como são variáveis com memórias diferentes, alterar um não vai mudar a memória do outro.
+// O c2 mudou o c1 pois como ali não tem um objeto e sim seu endereço de memória, o Carro c2 não foi criado, mas sim o c2
+//copiou o endereço de c1 e agora eles o compartilham.
+//Como agora os dois apontam para o mesmo objeto, o c2 consegue mudar o c1.
+
+// no exercicio 11 usando int o valor era guardado na STACK, assim fazendo uma cópia do valor, alterar um não afeta o outro,
+//pois são idependentes após a cópia.
+//no exercicio 12 que usa o tipo de referência, o objeto é armazenado no Heap e a Stack guarda só o endereço,
+//então quando se faz a cópia, ambos vão olhar para o mesmo lugar no Heap, e por conta disso, eles são dependentes.
